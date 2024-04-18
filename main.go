3f83679bd666dabc2a1b69fd1c9f0794c63f09ad
kzmnevgn <kzmnevgn@gmail.com>
2024-04-18 23:14:21 +0900

    add func main()

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+	//token = flags.Get(token)
+
+	//tgClient = telegram.New(token)
+
+	//fetcher = fetcher.New()
+
+	//processor = processor.New()
+
+	//consumer.Start(fetcher, processor)
+}
