eb00b1420674d22ee3a7a89ddfbd9774949c1c59
kzmnevgn <kzmnevgn@gmail.com>
2024-04-18 23:55:27 +0900

    upgrate main.go: add func mustToken()

--- main.go
+++ main.go
@@ -1,6 +1,13 @@
 package main
 
+import (
+	"flag"
+	"log"
+)
+
 func main() {
+	t := mustToken()
+
 	//token = flags.Get(token)
 
 	//tgClient = telegram.New(token)
@@ -10,4 +17,21 @@ func main() {
 	//processor = processor.New()
 
 	//consumer.Start(fetcher, processor)
+}
+
+func mustToken() string {
+	// bot -t-bot-token 'my token'
+	token := flag.String(
+		"token-bot-token", //name
+		"", //value
+		"token for access to telegram bot" //usage
+	)
+
+	flag.Parse()
+
+	if *token == "" {
+		log.Fatal("token is not specified")
+	}
+
+	return *token
 }
