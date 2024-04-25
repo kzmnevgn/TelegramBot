68c42a2f73f0659702f51e5f425d7b843ed80eb8
kzmnevgn <kzmnevgn@gmail.com>
2024-04-25 22:16:45 +0900

    update clients/telegram/telegram.go

--- clients/telegram/telegram.go
+++ clients/telegram/telegram.go
@@ -1,11 +1,14 @@
 package telegram
 
 import (
-	"fmt"
+	"encoding/json"
+	"io"
 	"net/http"
 	"net/url"
 	"path"
 	"strconv"
+
+	"read-adviser-bot/lib/e"
 )
 
 type Client struct {
@@ -14,8 +17,13 @@ type Client struct {
 	client   http.Client
 }
 
-func New(host string, token string) {
-	return Client{
+const (
+	getUpdatesMethod  = "getUpdates"
+	sendMessageMethod = "sendMessage"
+)
+
+func New(host string, token string) *Client {
+	return &Client{
 		host:     host,
 		basePath: newBasePath(token),
 		client:   http.Client{},
@@ -26,13 +34,43 @@ func newBasePath(token string) string {
 	return "bot" + token
 }
 
-func (c *Client) Updates(offset int, limit int) ([]Updates, error) {
+func (c *Client) Updates(offset int, limit int) (updates []Update, err error) {
+	defer func() { err = e.WrapIfErr("can't get updates", err) }()
+
 	q := url.Values{}
-	q.add("offset", strconv.Itoa(offset))
-	q.add("limit", strconv.Itoa(limit))
+	q.Add("offset", strconv.Itoa(offset))
+	q.Add("limit", strconv.Itoa(limit))
+
+	data, err := c.doRequest(getUpdatesMethod, q)
+	if err != nil {
+		return nil, err
+	}
+
+	var res UpdatesResponse
+
+	if err := json.Unmarshal(data, &res); err != nil {
+		return nil, err
+	}
+
+	return res.Result, nil
 }
 
-func (c *Client) doRequest(method string, query url.Values) ([]byte, error) {
+func (c *Client) SendMessage(chatID int, text string) error {
+	q := url.Values{}
+	q.Add("chat_id", strconv.Itoa(chatID))
+	q.Add("text", text)
+
+	_, err := c.doRequest(sendMessageMethod, q)
+	if err != nil {
+		return e.Wrap("can't send message", err)
+	}
+
+	return nil
+}
+
+func (c *Client) doRequest(method string, query url.Values) (data []byte, err error) {
+	defer func() { err = e.WrapIfErr("can't do request", err) }()
+
 	u := url.URL{
 		Scheme: "https",
 		Host:   c.host,
@@ -41,17 +79,21 @@ func (c *Client) doRequest(method string, query url.Values) ([]byte, error) {
 
 	req, err := http.NewRequest(http.MethodGet, u.String(), nil)
 	if err != nil {
-		return nil, fmt.Errorf("can't do request: %w", err)
+		return nil, err
 	}
 
 	req.URL.RawQuery = query.Encode()
 
-	resp, err := c.Client.Do(req)
+	resp, err := c.client.Do(req)
 	if err != nil {
-		return nil, fmt.Errorf("can't do request: %w", err)
+		return nil, err
 	}
-}
+	defer func() { _ = resp.Body.Close() }()
 
-func (c *Client) SendMessage() {
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
 
+	return body, nil
 }
